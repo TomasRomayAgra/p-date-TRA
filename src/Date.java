9759bd9b27d7e38c7b3a9c2d2bb0cf6992be3c22
TomasRomayAgra <Tromaa00@estudiantes.unileon.es>
2019-10-31 13:59:10 +0100

    Nueva version practica Date para programacion 1

---
+++ src/Date.java
@@ -0,0 +1,82 @@
+/**
+ * Clase date para la superacion de la practica 
+ * de Prog1 de la clase date
+ * 
+ * 
+ */
+public class Date {
+
+
+	private int day;
+	private int month;
+	private int year;
+	
+	/**
+	 * Constructor clase date
+	 * 
+	 */
+	public Date() {
+		// TODO Auto-generated constructor stub
+		this.day=1;
+		this.month=1;
+		this.year=1;
+	
+	}
+	public Date(int day,int month,int year) {
+		this.day=day;
+		this.month=month;
+		this.year=year;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+
+
+	public int getMonth() {
+		return month;
+	}
+
+
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Date [day=" + day + ", month=" + month + ", year=" + year + "]";
+	}
+
+	
+	public boolean isSameYear(Date miFecha) {
+		boolean isSameYear;
+		if(this.year==miFecha.getYear()) {
+			retorno=true;
+		}
+			
+		return isSameYear;
+	}
+	
+}
---
+++ src/MainDate.java
@@ -0,0 +1,14 @@
+
+public class MainDate {
+public static void main(String[] args) {
+	
+	Date myFecha1;
+	Date myFecha2;
+	if(myFecha.isSameYear(myFecha2)) {
+		System.out.println("Es el mismo year");
+	}
+	else {
+		System.out.println("Son years distintos");
+	}
+}
+}
